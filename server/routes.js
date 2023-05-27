3925fe45ee44c92b8a82f49f4d68eeef54c133b8
aitzeng <aitzeng@gmail.com>
2023-05-27 13:31:52 -0700

    Added metadata route

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const controller = require('./controllers/controlReviews');
 
-router.get('', controller.get);
+router.get('', controller.getRev);
+router.get('/meta', controller.getMeta)
 
 module.exports = router;
