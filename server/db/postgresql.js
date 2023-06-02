3aa4c244d706fcfe03ff7217de72cfab26f0d4b1
aitzeng <aitzeng@gmail.com>
2023-06-01 23:58:27 -0700

    Updated example.env and pool info

--- server/db/postgresql.js
+++ server/db/postgresql.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: 'localhost',
+  host: '54.219.122.7',
   port: process.env.postgreSQLPORT,
-  database: 'SDC',
+  database: 'sdc',
   user: process.env.postgreSQLUser,
   password: process.env.postgreSQLPassword,
 });
